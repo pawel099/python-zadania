1656da88b15a7ea683681d35b9e2c1e32ea57a7d
Pawel <pawelstawicki2@wp.pl>
2025-09-05 13:16:41 +0200

    change 05.09.2025

--- zadania/aplikacje/baza-uzytkowników/baza-uzytkownikow.py
+++ zadania/aplikacje/baza-uzytkowników/baza-uzytkownikow.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+ 
 import sqlite3
 import sys
 
@@ -71,92 +71,4 @@ for users in dane:
 
 
     
-
-
-=======
-
-import sqlite3
-import sys
-
-class baza:
-  
-  def __init__(self,polacz:str):
- 
-
-    self.connection = sqlite3.connect(polacz)
-    self.cursor = self.connection.cursor()
-    
-    
-    self.create_table()
-
-
-  def create_table(self):
-     
-     self.cursor.execute('''CREATE TABLE IF NOT EXISTS student (
-       id INTEGER PRIMARY KEY ,
-       name TEXT NOT NULL
-       )
-     ''')
-
-  def insert_user(self):
-
-    self.cursor.execute('''
-    INSERT INTO student (id, name)
-    VALUES(1,pawel)                   
-
-   '''
-
-
-    )
-    
-  def commit(self):
-
-    self.connection.commit()
-
-
-  def pobierzRekord(self,user:str):
-
-     self.cursor.execute('SELECT id ,name FROM student')
-     uzytkownik = self.cursor.fetchall()
-
-     for wynik in uzytkownik:
-      return wynik 
-
-  def close(self):
-
-    self.close()
-
-    
-
-polaczenie = baza('baza.sqlite')
-uzytkownik = int(input('wpisz id użytkownika: '))
-
-wynik = polaczenie.pobierzRekord(uzytkownik)
-
-print(wynik)
-
-
-  
-
-
-
-
-
-   
->>>>>>> ad0ec887eb8e5c6c29a01d248f5391e2e6239721
- 
- 
- 
- 
-
-
-
-
-
-
-
-
-
-
-
 
