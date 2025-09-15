d263b72dfda31ef29fce6e98d2d63ee1c9d42bac
Pawel <pawelstawicki2@wp.pl>
2025-09-15 14:31:44 +0200

    baza uzytkowników complete

--- zadania/aplikacje/baza-uzytkowników/baza-uzytkownikow.py
+++ zadania/aplikacje/baza-uzytkowników/baza-uzytkownikow.py
@@ -1,74 +1,7 @@
  
-import sqlite3
-import sys
+import engine
 
-class student:
-    
-    def __init__(self,baza):
-        self.connection = sqlite3.connect(baza)
-        self.cursor = self.connection.cursor()
-        
-
-    def createUser(self):
-     
-     users = [
-          
-     ]
-
-     sql_insert = "INSERT INTO student (id, name) VALUES (?, ?)"
-     self.cursor.executemany(sql_insert,users)
-     self.connection.commit()
-
-    def removeUser(self,user):
-     #self.cursor.execute(f"DELETE FROM student WHERE id={user}") 
-     print(f"DELETE * FROM student WHERE id ={user}")
-       
-
-      
-
-    def viewRekord(self):
-        
-        wynik = self.cursor.execute("SELECT * FROM student")   
-        table = wynik.fetchall()
-        return table
-    
-
-polacz = student('zadania\\aplikacje\\baza-uzytkowników\\baza-python.sqlite')
- 
-#polacz.createUser()
-input 
-polacz.removeUser(10)
-print("wybierz 1 by wyswietlić rekordy ..")
-print("wybierz 2 by usunać rekord .. ")
-dane = int(input())
-
-
-match dane:
-
- case 2:
-      
-      identyfikator = int(input(" podaj id uzytkownika "))
-      polacz.removeUser(identyfikator) 
-      print("usunieto uztkownika")
-      sys.exit(0)     
-
- case 1:
-      
-  dane = polacz.viewRekord()
-
-for users in dane:
-    print(users)
-
-   
-  
- 
- 
-
-
-  
-  
-
-
-
+class student(engine):
+    pass
     
 
